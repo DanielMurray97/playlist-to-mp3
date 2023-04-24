9d44abb7a5db21b5a603fb92da3d331ac78dfd2e
DanielMurray97 <daniel.murray@digital.cabinet-office.gov.uk>
2023-04-25 00:14:28 +0100

    added function to download songs from track name

--- youtube.py
+++ youtube.py
@@ -1,6 +1,8 @@
 import yt_dlp as youtube_dl
 from urllib import request as rq
 import os
+import pandas as pd
+import re
 
 
 def initizilze_yt_dlp(directory, vid_link):
@@ -32,3 +34,29 @@ def create_download_dir(dir_name):
 
     except OSError:
         print("Creation of the download directory failed")
+
+def download_tracks(playlist_dir):
+
+    songs = pd.read_csv(playlist_dir)
+    dir = create_download_dir('MP3')
+
+    i = 0
+    while i < len(songs["track"]):
+        this_song = songs["track"][i]+" " + songs["artist"][i] + ' ' + 'lyrics'
+        this_search = "+".join(this_song.strip().split())
+        div = rq.urlopen(
+            f"https://www.youtube.com/results?search_query={this_search}")
+
+        video_ids = re.findall(r"watch\?v=(\S{11})", div.read().decode())
+
+        if video_ids:
+            url = "https://www.youtube.com/watch?v=" + video_ids[0]
+
+            print(url)
+
+            initizilze_yt_dlp(dir, url)
+
+            i += 1
+
+if __name__ == '__main__':
+    pass
